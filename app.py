93f63cecc2773028f045435a9d50210be3b48a35
alikhan37544 <alikhan37544@gmail.com>
2025-03-21 09:10:25 +0530

    fix

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ def analyze_differences():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0,0,0,0', port=5000)
+    app.run(debug=True, threaded=True, host='0.0.0.0')
